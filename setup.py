c99387f67d1f41a851b04d15e59c58fa556e766a
Ahri Fox <89772420+AhriFoxSnek@users.noreply.github.com>
2024-01-08 01:11:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from ASnake import ASnakeVersion
+
 if __name__ == '__main__':
-    setup()
+    setup(
+    name='ASnake',
+    version=ASnakeVersion[1:],
+    author='Ahri Fox',
+    url='https://asnake.org',
+    description='Optimizing Python transpiler for the ASnake programming language.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'sly>=0.4',
+        'autopep8>=1.5.5',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ASnake=ASnake:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
